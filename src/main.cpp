fcc3a6409b23f4d47cf736842b62c1e5f9a9b49d
Drathijin <rsulbara@ucm.es>
2022-06-11 18:46:42 +0200

    Added libcurl and openssl

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <functional>
 #include <SDL.h>
-
+#include <curl/curl.h>
 
 int main(int argc, char** argv)
 {
@@ -14,7 +14,7 @@ int main(int argc, char** argv)
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         printf("Unable to init SDL : %s", SDL_GetError());
         return 1;
-    } window = SDL_CreateWindow("Launchy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    } window = SDL_CreateWindow("Launchy", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN|SDL_WINDOW_BORDERLESS);
     if (window == NULL) {
         printf("Unable to create window : %s", SDL_GetError());
         return 1;
@@ -25,6 +25,33 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    CURL* curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+
+    curl = curl_easy_init();
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, "https://raw.githubusercontent.com/ChachiGames/launchy/main/README.md");
+
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYSTATUS, 1);
+        curl_easy_setopt(curl, CURLOPT_CAINFO, ".\\cacert.pem");
+        curl_easy_setopt(curl, CURLOPT_CAPATH, ".\\cacert.pem");
+
+        /* Perform the request, res will get the return code */
+        res = curl_easy_perform(curl);
+        /* Check for errors */
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
     // Infinite loop
     while (isRunning) {
 
@@ -34,6 +61,10 @@ int main(int argc, char** argv)
             if (event.type == SDL_QUIT) {
                 isRunning = false;
             }
+            if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
+                isRunning = false;
+            }
+
         }
 
         // Clear screen
