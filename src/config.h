2562e2d65a9ed2e0b5d8acea6f2cb0fc565a8bd3
Drathijin <rsulbara@ucm.es>
2022-06-12 21:21:10 +0200

    adding task for buidl and run

--- src/config.h
+++ src/config.h
@@ -2,9 +2,9 @@
 #define CONFIGURE_H_IN
 
 #define PROJECT_NAME "launchy"
-#define PROJECT_VER  "0.0.2"
+#define PROJECT_VER  "0.0.3"
 #define PROJECT_VER_MAJOR "0"
 #define PROJECT_VER_MINOR "0"
-#define PROJECT_VER_PATCH "2"
+#define PROJECT_VER_PATCH "3"
 
 #endif // CONFIGURE_H_IN
