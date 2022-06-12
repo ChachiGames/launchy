3f2d09f7fbde581a89bf60899f3d9a637e824965
Drathijin <rsulbara@ucm.es>
2022-06-12 12:45:22 +0200

    Added patcher as a wrapper for our launcher so that it can selfupdate

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,25 @@
 
 #include <iostream>
 #include <functional>
-
+#include <filesystem>
+#include <process.h>
 #include <string>
 
 #include "config.h"
+
+
+std::string global_string;
+size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata)
+{
+    size_t realsize = size * nmemb;
+    global_string.append(ptr, realsize);
+    return realsize;
+}
+
+size_t write_data(void* ptr, size_t size, size_t nmemb, FILE* stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
 
 int main(int argc, char** argv)
 {
@@ -40,24 +55,80 @@ int main(int argc, char** argv)
     curl = curl_easy_init();
 
     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://raw.githubusercontent.com/ChachiGames/launchy/main/README.md");
+        curl_easy_setopt(curl, CURLOPT_URL, "https://chachigames.github.io/launchy/manifest");
 
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYSTATUS, 1);
         curl_easy_setopt(curl, CURLOPT_CAINFO, ".\\cacert.pem");
         curl_easy_setopt(curl, CURLOPT_CAPATH, ".\\cacert.pem");
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 
         /* Perform the request, res will get the return code */
         res = curl_easy_perform(curl);
+        
+        auto split = [](std::string s, std::string delimiter) {
+            size_t pos = 0;
+            std::vector<std::string> tokens;
+            while ((pos = s.find(delimiter)) != std::string::npos) {
+                std::string token = s.substr(0, pos);
+                tokens.push_back(token);
+                s.erase(0, pos + delimiter.length());
+
+            };
+            tokens.push_back(s);
+            return tokens;
+        };
+
         /* Check for errors */
         if (res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
+        else
+        {
+            auto splits = split(global_string, "\n");
+            std::string version = splits[0];
+            std::string link = splits[1];
+            
 
+            auto numbers = split(version, ".");
+            if (numbers[0] > PROJECT_VER_MAJOR || numbers[1] > PROJECT_VER_MINOR || numbers[2] > PROJECT_VER_PATCH)
+            {
+                FILE* fp = fopen((std::filesystem::current_path().string() + "/newVersion.exe").c_str(), "wb");
+                curl_easy_setopt(curl, CURLOPT_URL, link.c_str());
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+                curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+                res = curl_easy_perform(curl);
+                if (res != CURLE_OK)
+                    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+            }
+
+            std::string thisFile = argv[0];   if (thisFile.find(".exe") == std::string::npos) thisFile += ".exe";
+            std::string oldVersion = std::filesystem::current_path().string() + "/oldVersion.exe";
+
+            // If there is an old version, it is removed
+            if (std::filesystem::exists(oldVersion)){
+                remove(oldVersion.c_str());
+            }
+
+            std::string newVersion = std::filesystem::current_path().string() + "/newVersion.exe";
+
+            // there is a newer version
+            if (std::filesystem::exists(newVersion))
+            {
+                // Versions are switched
+                rename(thisFile.c_str(), "oldVersion.exe");
+                SDL_DestroyWindow(window);
+                SDL_DestroyRenderer(renderer);
+                SDL_Quit();
+                curl_global_cleanup();
+
+                return 2; //Exit with our own code for re-launch
+            }
+        }
         /* always cleanup */
-        curl_easy_cleanup(curl);
+        curl_easy_cleanup(curl);       
     }
 
-    curl_global_cleanup();
 
     // Infinite loop
     while (isRunning) {
---
+++ src/patcher.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <filesystem>
+#include <process.h>
+#include <iostream>
+
+#include <Windows.h>
+
+int exc(std::string cmd, const char* v[]) {
+    STARTUPINFOA si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+    int i = 0;
+    while (v[i+1])
+        cmd.append(v[++i]);
+
+    CreateProcessA(0, cmd.data(), 0, 0, 0, 0, 0, 0, &si, &pi);
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    DWORD exit_code;
+    if (FALSE == GetExitCodeProcess(pi.hProcess, &exit_code))
+    {
+        std::cerr << "GetExitCodeProcess() failure: " <<
+            GetLastError() << "\n";
+    }
+    else if (STILL_ACTIVE == exit_code)
+    {
+        std::cout << "Still running\n";
+    }
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    return exit_code;
+}
+
+int main(int argc, char** argv)
+{
+    std::cout << "HJI"<<std::endl;
+    std::string newVersion = std::filesystem::current_path().string() + "/newVersion.exe";
+    std::string oldVersion = std::filesystem::current_path().string() +"/oldVersion.exe";
+    std::string launchy = std::filesystem::current_path().string() + "\\launchy.exe";
+    int res = 0;
+    while (res == 0 || res == 2)
+    {
+        if (std::filesystem::exists(newVersion))
+        {
+            if (std::filesystem::exists(oldVersion))
+                remove(oldVersion.c_str());
+
+            if (rename(newVersion.c_str(), "launchy.exe") != 0)
+            {
+                std::cout << "Could not rename to launchy" << errno << std::endl;
+                return 1;
+            };
+            const char* v[] = { launchy.c_str(), nullptr };
+            int lRes = exc(launchy.c_str(), v);
+            return 0;
+        }
+        else if (std::filesystem::exists(launchy))
+        {
+            const char* v[] = { launchy.c_str(), nullptr };
+            int lRes = exc(launchy.c_str(), v);
+            if (lRes == 0)
+                return 0;
+            else if (res == 0)
+                res = lRes;
+            else
+                return 0;
+        }
+    }
+    std::cout << "Error: launchy not found";
+    return 1;
+}
