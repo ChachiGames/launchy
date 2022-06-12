a67d25941a84296ab3f897846e96a405d5e27ca9
Drathijin <rsulbara@ucm.es>
2022-06-12 14:45:34 +0200

    Adding new version to manifest

--- src/config.h
+++ src/config.h
@@ -2,9 +2,9 @@
 #define CONFIGURE_H_IN
 
 #define PROJECT_NAME "launchy"
-#define PROJECT_VER  "0.0.0"
+#define PROJECT_VER  "0.0.1"
 #define PROJECT_VER_MAJOR "0"
 #define PROJECT_VER_MINOR "0"
-#define PROJECT_VER_PATCH "0"
+#define PROJECT_VER_PATCH "1"
 
 #endif // CONFIGURE_H_IN
